8e3c4be9e742c614a298d3e996a3d1ebf1c9a863
bogle-zhao <developer.zhao@qq.com>
2019-05-08 22:46:18 +0800

    文件锁定

--- src/main/java/com/shengsiyuan/netty/nio/NioTest11.java
+++ src/main/java/com/shengsiyuan/netty/nio/NioTest11.java
@@ -7,6 +7,7 @@ import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 
 /**
+ * 实现对文件的锁定
  * @author bogle
  * @version 1.0 2019/3/18 下午10:02
  */
@@ -15,9 +16,9 @@ public class NioTest11 {
     public static void main(String[] args) throws IOException {
         RandomAccessFile randomAccessFile = new RandomAccessFile("NioTest11.txt", "rw");
         FileChannel fileChannel = randomAccessFile.getChannel();
-        FileLock fileLock = fileChannel.lock(3, 6, true);
+        FileLock fileLock = fileChannel.lock(3, 6, true/*true:共享锁，false：排他锁*/);
         System.out.println("valid:" + fileLock.isShared());
-        System.out.println("lock type: " + fileLock.isShared());
+        System.out.println("lock type: " + fileLock.isShared());//是否为共享锁
 
         fileLock.release();
         randomAccessFile.close();
